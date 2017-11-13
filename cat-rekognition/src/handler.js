e335e2878a476751f9d9943c23f833552e43f047
Sebastian Feduniak <sebastian.feduniak@gmail.com>
2017-11-13 14:00:14 +0100

    #4: dynamodb integration

--- cat-rekognition/src/handler.js
+++ cat-rekognition/src/handler.js
@@ -1,16 +1,21 @@
 'use strict';
 
+const STATUS_TABLE_NAME = 'CatDetectionStatus'
+
 const AWS = require('aws-sdk');
 
 const rekognition = new AWS.Rekognition();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.imgClassification = (event, context, callback) => {
 
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    const filesToBeChecked = this.recordsToFiles(this.filterEvents(event));
+    const filesToBeChecked = module.exports.recordsToFiles(module.exports.filterEvents(event));
 
     filesToBeChecked.forEach(function (fileName) {
+        putNewFileStatus(fileName);
+
         var params = {
             Image: {
                 S3Object: {
@@ -23,9 +28,20 @@ module.exports.imgClassification = (event, context, callback) => {
         };
 
         rekognition.detectLabels(params, function(err, data) {
-            if (err) console.log(err, err.stack);
-            else     console.log(data);
-        });
+                if (err) {
+                    console.log(err, err.stack);
+                    putFileErrorStatus(fileName);
+                }
+                else {
+                    console.log(data);
+                    const isCat = module.exports.isCatRecognized(data);
+                    if (isCat) {
+                        putCatRecognizedStatus(fileName);
+                    } else {
+                        putOtherFileStatus(fileName);
+                    }
+                }
+            });
     });
 
     callback(null, event);
@@ -46,4 +62,44 @@ module.exports.recordsToFiles = (s3Records) => {
         return record['s3']['object']['key'];
     });
 };
+
+module.exports.isCatRecognized = (rawLabels) => {
+    const labels = rawLabels.Labels;
+    return labels.some(function (label) { return label.Name == 'Cat' });
+};
+
+module.exports.dbItem = (fileName, scanned, scanningStatus) => {
+    return {
+        TableName: STATUS_TABLE_NAME,
+        Item: {
+            'name': fileName,
+            'checked': scanned,
+            'status': scanningStatus
+        }
+    };
+};
+
+function putNewFileStatus(fileName) {
+    return putStatus(module.exports.dbItem(fileName, false, 'new'));
+};
+
+function putFileErrorStatus(fileName) {
+    return putStatus(module.exports.dbItem(fileName, true, 'error'));
+};
+
+function putCatRecognizedStatus(fileName) {
+    return putStatus(module.exports.dbItem(fileName, true, 'cat'));
+};
+
+function putOtherFileStatus(fileName) {
+    return putStatus(module.exports.dbItem(fileName, true, 'other'));
+};
+
+function putStatus(status) {
+    dynamoDb.put(status, function(err, data) {
+        if (err) console.log(err, err.stack);
+        else     console.log(data);
+    });
+};
+
 
--- cat-rekognition/test/test-processing.js
+++ cat-rekognition/test/test-processing.js
@@ -4,21 +4,21 @@ var chai = require('chai'),
 
 const lambda = require("../src/handler.js");
 
-describe('Test processing', function() {
+describe('Test processing', function () {
     it('should accept create event', function () {
-        const objectCreatedEvent = '{ "Records":[ { "eventVersion":"2.0", "eventSource":"aws:s3", "awsRegion":"eu-west-1", "eventTime":"2017-11-07T14:48:17.640Z", "eventName":"ObjectCreated:Put", "userIdentity":{ "principalId":"AWS:AIDAIO4UECELEIKQL727W" }, "requestParameters":{ "sourceIPAddress":"64.236.4.1" }, "responseElements":{ "x-amz-request-id":"E9539EA0992FE185", "x-amz-id-2":"i9fnN8mAGRF/AR4TSV4hcCSWn0b3tag2Gy//j3PyYhtxzMgIwEVOJoSBo9SW7mpwX7cRc24eq1M=" }, "s3":{ "s3SchemaVersion":"1.0", "configurationId":"4e51601f-b26b-4b2f-9389-f39da91a1890", "bucket":{ "name":"cat-detector-img-repo", "ownerIdentity":{ "principalId":"A3W0C5E4PDB3VB" }, "arn":"arn:aws:s3:::cat-detector-img-repo" }, "object":{ "key":"cat1.jpeg", "size":12920, "eTag":"4a421caed9813b5b620f9e777a43caa8", "sequencer":"005A01C7B17DA1680E" } } } ] }'
+        const objectCreatedEvent = '{ "Records":[ { "eventVersion":"2.0", "eventSource":"aws:s3", "awsRegion":"eu-west-1", "eventTime":"2017-11-07T14:48:17.640Z", "eventName":"ObjectCreated:Put", "userIdentity":{ "principalId":"AWS:AIDAIO4UECELEIKQL727W" }, "requestParameters":{ "sourceIPAddress":"64.236.4.1" }, "responseElements":{ "x-amz-request-id":"E9539EA0992FE185", "x-amz-id-2":"i9fnN8mAGRF/AR4TSV4hcCSWn0b3tag2Gy//j3PyYhtxzMgIwEVOJoSBo9SW7mpwX7cRc24eq1M=" }, "s3":{ "s3SchemaVersion":"1.0", "configurationId":"4e51601f-b26b-4b2f-9389-f39da91a1890", "bucket":{ "name":"cat-detector-img-repo", "ownerIdentity":{ "principalId":"A3W0C5E4PDB3VB" }, "arn":"arn:aws:s3:::cat-detector-img-repo" }, "object":{ "key":"cat1.jpeg", "size":12920, "eTag":"4a421caed9813b5b620f9e777a43caa8", "sequencer":"005A01C7B17DA1680E" } } } ] }';
         var filtered = lambda.filterEvents(JSON.parse(objectCreatedEvent));
 
         expect(filtered).to.have.lengthOf(1);
     });
     it('should reject delete event', function () {
-        const objectCreatedEvent = '{ "Records":[ { "eventVersion":"2.0", "eventSource":"aws:s3", "awsRegion":"eu-west-1", "eventTime":"2017-11-07T14:48:17.640Z", "eventName":"ObjectRemoved:Delete", "userIdentity":{ "principalId":"AWS:AIDAIO4UECELEIKQL727W" }, "requestParameters":{ "sourceIPAddress":"64.236.4.1" }, "responseElements":{ "x-amz-request-id":"E9539EA0992FE185", "x-amz-id-2":"i9fnN8mAGRF/AR4TSV4hcCSWn0b3tag2Gy//j3PyYhtxzMgIwEVOJoSBo9SW7mpwX7cRc24eq1M=" }, "s3":{ "s3SchemaVersion":"1.0", "configurationId":"4e51601f-b26b-4b2f-9389-f39da91a1890", "bucket":{ "name":"cat-detector-img-repo", "ownerIdentity":{ "principalId":"A3W0C5E4PDB3VB" }, "arn":"arn:aws:s3:::cat-detector-img-repo" }, "object":{ "key":"cat1.jpeg", "size":12920, "eTag":"4a421caed9813b5b620f9e777a43caa8", "sequencer":"005A01C7B17DA1680E" } } } ] }'
+        const objectCreatedEvent = '{ "Records":[ { "eventVersion":"2.0", "eventSource":"aws:s3", "awsRegion":"eu-west-1", "eventTime":"2017-11-07T14:48:17.640Z", "eventName":"ObjectRemoved:Delete", "userIdentity":{ "principalId":"AWS:AIDAIO4UECELEIKQL727W" }, "requestParameters":{ "sourceIPAddress":"64.236.4.1" }, "responseElements":{ "x-amz-request-id":"E9539EA0992FE185", "x-amz-id-2":"i9fnN8mAGRF/AR4TSV4hcCSWn0b3tag2Gy//j3PyYhtxzMgIwEVOJoSBo9SW7mpwX7cRc24eq1M=" }, "s3":{ "s3SchemaVersion":"1.0", "configurationId":"4e51601f-b26b-4b2f-9389-f39da91a1890", "bucket":{ "name":"cat-detector-img-repo", "ownerIdentity":{ "principalId":"A3W0C5E4PDB3VB" }, "arn":"arn:aws:s3:::cat-detector-img-repo" }, "object":{ "key":"cat1.jpeg", "size":12920, "eTag":"4a421caed9813b5b620f9e777a43caa8", "sequencer":"005A01C7B17DA1680E" } } } ] }';
         var filtered = lambda.filterEvents(JSON.parse(objectCreatedEvent));
 
         expect(filtered).to.be.empty;
     });
     it('should retrieve file name', function () {
-        const objectCreatedEvent = '{ "Records":[ { "eventVersion":"2.0", "eventSource":"aws:s3", "awsRegion":"eu-west-1", "eventTime":"2017-11-07T14:48:17.640Z", "eventName":"ObjectCreated:Put", "userIdentity":{ "principalId":"AWS:AIDAIO4UECELEIKQL727W" }, "requestParameters":{ "sourceIPAddress":"64.236.4.1" }, "responseElements":{ "x-amz-request-id":"E9539EA0992FE185", "x-amz-id-2":"i9fnN8mAGRF/AR4TSV4hcCSWn0b3tag2Gy//j3PyYhtxzMgIwEVOJoSBo9SW7mpwX7cRc24eq1M=" }, "s3":{ "s3SchemaVersion":"1.0", "configurationId":"4e51601f-b26b-4b2f-9389-f39da91a1890", "bucket":{ "name":"cat-detector-img-repo", "ownerIdentity":{ "principalId":"A3W0C5E4PDB3VB" }, "arn":"arn:aws:s3:::cat-detector-img-repo" }, "object":{ "key":"cat1.jpeg", "size":12920, "eTag":"4a421caed9813b5b620f9e777a43caa8", "sequencer":"005A01C7B17DA1680E" } } } ] }'
+        const objectCreatedEvent = '{ "Records":[ { "eventVersion":"2.0", "eventSource":"aws:s3", "awsRegion":"eu-west-1", "eventTime":"2017-11-07T14:48:17.640Z", "eventName":"ObjectCreated:Put", "userIdentity":{ "principalId":"AWS:AIDAIO4UECELEIKQL727W" }, "requestParameters":{ "sourceIPAddress":"64.236.4.1" }, "responseElements":{ "x-amz-request-id":"E9539EA0992FE185", "x-amz-id-2":"i9fnN8mAGRF/AR4TSV4hcCSWn0b3tag2Gy//j3PyYhtxzMgIwEVOJoSBo9SW7mpwX7cRc24eq1M=" }, "s3":{ "s3SchemaVersion":"1.0", "configurationId":"4e51601f-b26b-4b2f-9389-f39da91a1890", "bucket":{ "name":"cat-detector-img-repo", "ownerIdentity":{ "principalId":"A3W0C5E4PDB3VB" }, "arn":"arn:aws:s3:::cat-detector-img-repo" }, "object":{ "key":"cat1.jpeg", "size":12920, "eTag":"4a421caed9813b5b620f9e777a43caa8", "sequencer":"005A01C7B17DA1680E" } } } ] }';
         const fileAddedRecord = JSON.parse(objectCreatedEvent)[`Records`];
 
         var fileNames = lambda.recordsToFiles(fileAddedRecord);
@@ -26,4 +26,35 @@ describe('Test processing', function() {
         expect(fileNames).to.have.lengthOf(1);
         expect(fileNames[0]).to.equal('cat1.jpeg');
     });
+    it("should rekognize a cat", function () {
+        const rawLabels = {
+            'Labels': [
+                { 'Name': 'Animal', 'Confidence': 72.16043853759766 },
+                { 'Name': 'Cat', 'Confidence': 72.16043853759766 },
+                { 'Name': 'Pet', 'Confidence': 72.16043853759766 }
+            ]
+        };
+
+        var isACat = lambda.isCatRecognized(rawLabels);
+
+        expect(isACat).to.be.true;
+    })
+    it("should not rekognize a cat", function () {
+        const rawLabels = {
+            'Labels': [
+                { 'Name': 'Animal', 'Confidence': 72.16043853759766 },
+                { 'Name': 'Dog', 'Confidence': 72.16043853759766 },
+                { 'Name': 'Pet', 'Confidence': 72.16043853759766 }
+            ]
+        };
+
+        var isACat = lambda.isCatRecognized(rawLabels);
+
+        expect(isACat).to.be.false;
+    })
+    it("should create data item", function () {
+        var newFile = lambda.dbItem('file1', false, 'new');
+
+        expect(newFile.Item.status).to.equal('new');
+    })
 });
