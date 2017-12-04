f2c06a3be751023338dab06246c4e00ac84342d4
Wojciech Gawronski <afronski@gmail.com>
2017-12-04 12:27:28 +0100

    Improvements for CF templates creating users.
    This closes #1.

--- infrastructure/template.sh
+++ infrastructure/template.sh
@@ -9,41 +9,9 @@ cat <<EOF
   User${ACTUAL}:
     Type: AWS::IAM::User
     Properties:
-      UserName: "serverless-cat-reader-user${ACTUAL}"
+      UserName: "serverless-cat-detector-user${ACTUAL}"
       LoginProfile:
         Password: !Ref "PASSWORD"
         PasswordResetRequired: true
 
-  User${ACTUAL}Bucket:
-    Type: AWS::S3::Bucket
-    Properties:
-      AccessControl: Private
-      BucketName: "serverless-cat-reader-storage-user${ACTUAL}"
-      CorsConfiguration:
-        CorsRules:
-          - AllowedHeaders: ["*"]
-            AllowedMethods: ["GET", "PUT"]
-            AllowedOrigins: ["*"]
-      Tags:
-        - Key: Origin
-          Value: Workshop-Cloudyna-2017
-
-  User${ACTUAL}Table:
-    Type: "AWS::DynamoDB::Table"
-    DependsOn:
-      - User${PREVIOUS}Table
-    Properties:
-      AttributeDefinitions:
-        - AttributeName: "name"
-          AttributeType: S
-      KeySchema:
-        - AttributeName: "name"
-          KeyType: HASH
-      ProvisionedThroughput:
-        ReadCapacityUnits: 1
-        WriteCapacityUnits: 1
-      TableName: "serverless-cat-reader-table-user${ACTUAL}"
-      Tags:
-        - Key: Origin
-          Value: Workshop-Cloudyna-2017
 EOF
